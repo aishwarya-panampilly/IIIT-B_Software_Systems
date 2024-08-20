1a457d55c757495c1ee26be8d8e12e631d1562d5
aishwarya-panampilly <aishwarya.panampilly@gmail.com>
2024-08-20 14:30:45 +0530

    Second Program

---
+++ Program_2.c
@@ -0,0 +1,21 @@
+/***************************************************************************************************
+ *Name: Aishwarya J Panampilly
+ *Reg.No: MT2024011
+ *Problem Statement : Write a simple program to execute in an infinite loop at the background. Go
+                      to /proc directory and identify all the process related information in the  
+                      corresponding proc directory. 
+***************************************************************************************************/
+
+/******************************************SOLUTION************************************************/
+#include <stdio.h>
+int main()
+{
+  while (1) {
+    //simple infinite loop 
+    }
+    return 0;
+}
+// output with "./a.out & " to get pid then cd to home, cd to /proc/pid to reach process then ls
+// run ps aux | grep hop2.c to see the pid of process... ls /proc/pid to list process
+
+
