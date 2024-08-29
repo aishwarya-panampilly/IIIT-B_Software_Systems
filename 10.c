d54c1d0108b138d9143281da82243efdcf278aa2
aishwarya-panampilly <aishwarya.panampilly@gmail.com>
2024-08-30 01:47:02 +0530

    Hardcoded program 10

--- 10.c
+++ 10.c
@@ -1,109 +1,77 @@
 /***************************************************************************************************
- *Name: 10.c
- *Author: Aishwarya J Panampilly
+ *Name: Aishwarya J Panampilly
  *Reg.No: MT2024011
- *Description: Write a program to open a file with read write mode, write 10 bytes, move the file pointer by 10 bytes (use lseek) and write again 10 bytes.
-               a. check the return value of lseek
-               b. open the file with od and check the empty spaces in between the data.
- *Date: 29th August,2024
+ *Problem Statement : Write a program to open a file with read write mode, write 10 bytes, move the  
+                      file pointer by 10 bytes (use lseek) and write again 10 bytes.
+                      a. check the return value of lseek
+                      b. open the file with od and check the empty spaces in between the data.
 ***************************************************************************************************/
 
 /******************************************SOLUTION************************************************/
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
-
-#define BUFFER_SIZE 256
-#define DATA_SIZE 10
-
 
 int main() {
-    char filename[BUFFER_SIZE];
-    char first_data[DATA_SIZE + 1];  // +1 for null terminator
-    char second_data[DATA_SIZE + 1]; // +1 for null terminator
-
-    printf("Enter the filename: ");
-    fflush(stdout);
-    ssize_t bytes_read = read(STDIN_FILENO, filename, sizeof(filename) - 1);  // Leave space for null terminator
-    if (bytes_read <= 0) {
-        perror("Error reading filename");
-        return 1;
-    }
-
-    // Null-terminate the string and remove newline character if present
-    filename[bytes_read] = '\0';
-    if (filename[bytes_read - 1] == '\n') {
-        filename[bytes_read - 1] = '\0';
-    }
-
-    printf("Enter the first 10 bytes to write: ");
-    fflush(stdout);
-    bytes_read = read(STDIN_FILENO, first_data, DATA_SIZE);
-    if (bytes_read != DATA_SIZE) {
-        perror("Error reading first data");
-        return 1;
-    }
-
-    // Null-terminate the first data string
-    first_data[DATA_SIZE] = '\0';
-    if (first_data[DATA_SIZE - 1] == '\n') {
-        first_data[DATA_SIZE - 1] = '\0';
-    }
-
-    // Get second 10 bytes of data from user
-    printf("Enter the second 10 bytes to write: ");
-    fflush(stdout);
-    bytes_read = read(STDIN_FILENO, second_data, DATA_SIZE);
-    if (bytes_read != DATA_SIZE) {
-        perror("Error reading second data");
-        return 1;
-    }
-
-    // Null-terminate the second data string
-    second_data[DATA_SIZE] = '\0';
-    if (second_data[DATA_SIZE - 1] == '\n') {
-        second_data[DATA_SIZE - 1] = '\0';
-    }
-
-    // Open the file in read-write mode, create it if it does not exist
-    int fd = open(filename, O_RDWR | O_CREAT, 0666);
+    // Open the file in read-write mode ,create if it doesn't exist
+    int fd = open("testfile.txt", O_RDWR | O_CREAT, 0644);
     if (fd == -1) {
         perror("Error opening file");
         return 1;
     }
 
-    // First write: 10 bytes
-    ssize_t bytes_written = write(fd, first_data, DATA_SIZE);
-    if (bytes_written != DATA_SIZE) {
+    // Write 10 bytes to the file
+    const char *data1 = "ABCDEFGHIJ";
+    ssize_t bytes_written = write(fd, data1, 10);
+    if (bytes_written != 10) {
         perror("Error writing first 10 bytes");
         close(fd);
         return 1;
     }
 
-    // Move the file pointer by 10 bytes using lseek
+    // Move the file pointer 10 bytes forward
     off_t offset = lseek(fd, 10, SEEK_CUR);
     if (offset == (off_t)-1) {
-        perror("Error using lseek");
+        perror("Error with lseek");
         close(fd);
         return 1;
+    } else {
+        printf("lseek succeeded. Current file offset: %ld\n", offset);
     }
-    
-    // Print the new file pointer position after lseek
-    printf("File pointer moved to byte offset: %ld\n", (long)offset);
-    fflush(stdout);
 
-    // Second write: 10 bytes
-    bytes_written = write(fd, second_data, DATA_SIZE);
-    if (bytes_written != DATA_SIZE) {
+    // Write another 10 bytes after the gap
+    const char *data2 = "KLMNOPQRST";
+    bytes_written = write(fd, data2, 10);
+    if (bytes_written != 10) {
         perror("Error writing second 10 bytes");
         close(fd);
         return 1;
     }
 
-    // Close the file
+    // Close the file descriptor
     close(fd);
+
+    printf("Write operations completed. Use 'od -c testfile.txt' to inspect the file.\n");
 
     return 0;
 }
+
+/******************************************OUTPUT************************************************/
+/*
+  aishjp@Aish-Linux:~/my-repo$ gcc Program_10.c
+  aishjp@Aish-Linux:~/my-repo$ ./a.out
+  lseek succeeded. Current file offset: 20
+  Write operations completed. Use 'od -c testfile.txt' to inspect the file.
+  aishjp@Aish-Linux:~/my-repo$ od -c testfile.txt
+  0000000   A   B   C   D   E   F   G   H   I   J   t   h       o   r   i
+  0000020   g   i   n   a   K   L   M   N   O   P   Q   R   S   T   c   r
+  0000040   i   p   t   o   r  \n   W   r   i   t   i   n   g       w   i
+  0000060   t   h       d   u   p       f   i   l   e       d   e   s   c
+  0000100   r   i   p   t   o   r  \n   W   r   i   t   i   n   g       w
+  0000120   i   t   h       d   u   p   2       f   i   l   e       d   e
+  0000140   s   c   r   i   p   t   o   r  \n   W   r   i   t   i   n   g
+  0000160       w   i   t   h       f   c   n   t   l       f   i   l   e
+  0000200       d   e   s   c   r   i   p   t   o   r  \n
+  0000214
+*/
+
